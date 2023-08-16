16b5befabed27a311f10db21182fe9c50ee59a73
JHuelsebusch <jhuelsebusch@gmx.de>
2023-08-16 11:18:57 +0200

    task_1_3

--- task_1.js
+++ task_1.js
@@ -1,19 +1,20 @@
 const input = [
-    { brand: "BMW", model: " 320 i", kw: 120 },
-    { brand: " Mercedes ", model: "C 200 ", kw: 134 },
-    { brand: " Renault ", model: " Laguna ", kw: 97 },
-    { brand: " Toyota ", model: " Yaris ", kw: 52 },
-    { brand: " Ford ", model: " Kuga ", kw: 85 },
-    { brand: " Volvo ", model: "V60", kw: 111 },
-    { brand: " Audi ", model: "A3", kw: 123 },
-    { brand: "VW", model: " Golf ", kw: 106 }
+    { brand: "BMW", model: "320i", kw: 120 },
+    { brand: "Mercedes", model: "C 200", kw: 134 },
+    { brand: "Renault", model: "Laguna", kw: 97 },
+    { brand: "Toyota", model: "Yaris", kw: 52 },
+    { brand: "Ford", model: "Kuga", kw: 85 },
+    { brand: "Volvo", model: "V60", kw: 111 },
+    { brand: "Audi", model: "A3", kw: 123 },
+    { brand: "VW", model: "Golf ", kw: 106 }
 ];
 
-let carsOver100Kw = [];
+let carsMoreKw = [];
 
 function initTask1() {
     sortByKw();
     extractByKw(100);
+    outputModels();
 }
 
 function sortByKw() {
@@ -25,12 +26,20 @@ function extractByKw(kw) {
     for (let i = 0; i < input.length; i++) {
         const car = input[i];
         if (car.kw > kw) {
-            carsOver100Kw.push(car);
+            carsMoreKw.push(car);
         }
     }
-    console.log(carsOver100Kw);
+    console.log(carsMoreKw);
 }
 
 function outputModels() {
-
+    let output = "";
+    for (let i = 0; i < input.length; i++) {
+        const car = input[i];
+        output += car.brand + " " + car.model;
+        if (i != input.length - 1) {
+            output += ", ";
+        }
+        console.log(output);
+    }
 }
