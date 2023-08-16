47da38e851d247cb84932175e7b035f2f8bea5a2
JHuelsebusch <jhuelsebusch@gmx.de>
2023-08-16 11:48:16 +0200

    task_2_3

--- task_2.js
+++ task_2.js
@@ -2,12 +2,15 @@ const arrayOne = ['a', 4, 56, 'q', 'b', 2, 7, 77, 'b', 'x', 1, 'h', 0, 12];
 const arrayTwo = [8, 2, 54, 'd', 'g', 3, 'j', 5, 6, 't', 7, 8, 's', 'b', 12, 345];
 
 let arrayNumbers = [];
+let uniqueNumbers = [];
 
 function initTask2() {
-    extractNumbers();
+    extractUniqueNumbers();
 }
 
-function extractNumbers() {
-    arrayNumbers = arrayOne.filter(e => +e).concat(arrayTwo.filter(e => +e));
-    console.log(arrayNumbers);
+function extractUniqueNumbers() {
+    arrayNumbers = arrayOne.filter(e => +e).concat(arrayTwo.filter(e => +e)); // filter all numbers and combine two arrays
+    arrayNumbers = arrayNumbers.filter(function(item, pos) { // removes duplicated numbers
+        return arrayNumbers.indexOf(item) == pos;
+    })
 }
